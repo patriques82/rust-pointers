4042f4f0cce9f8c171ec0cf61a46d344e8b111da
Patrik Nygren <patriknygren82@gmail.com>
2019-09-26 08:34:42 +0200

    Removed solutions

--- ex-14-sol.rs
+++
@@ -1,8 +0,0 @@
-use std::cell::RefCell;
-
-fn main() {
-    let mut hello = RefCell::new("World");
-    let mut world = RefCell::new("Hello");
-    hello.swap(&world);
-    println!("{}, {}", hello.get_mut(), world.get_mut());
-}
